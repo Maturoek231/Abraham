1a6ca722753368cdc2964f0563a38c2803ce0a2d
sanga lawalata <sanga.lawalata@gmail.com>
2019-09-03 21:35:19 +0700

    Add sketchbook to play for demo

---
+++ Sketchbook.py
@@ -0,0 +1,34 @@
+print("Hello World")  ## to print string to console or screen
+
+name = "John Doe"
+age = 17
+
+# To construct a string with some values with a place holder
+print("Nama : %s, Umur : %d" % (name, age))
+
+# john = {
+#     "name": "John",
+#     "age": 17
+# }
+#
+# maria = {
+#     "name": "Maria",
+#     "age": 24
+# }
+#
+# students = [];
+#
+# students.append(maria)
+# students.append(john)
+#
+# for student in students:
+#     print(student)
+#     print(student["name"])
+#     print(student["age"])
+#
+#     student_info = "Name: %, Age: %"
+#
+# students.sort(key=lambda x: x["name"])
+#
+# for s in students:
+#     print(s)
